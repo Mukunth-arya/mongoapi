c26760d1a3dc59d7496e744ae2ea7ae59eb495d5
Mukunth-arya <mukunthnataraj@gmail.com>
2022-02-15 22:47:04 +0530

    version4

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/gorilla/mux"
 
-	"github.com/Mukunth-arya/mongoapi/handler"
+	"github.com/Mukunth-arya/mongoapi/router"
 )
 
 func main() {
@@ -14,9 +14,9 @@ func main() {
 	fmt.Println("Here we are gooing to consruct the new api")
 	fmt.Println("Api for review system")
 	r := mux.NewRouter()
-	r.HandleFunc("/getnew", handler.Getalldata).Methods("GET")
-	r.HandleFunc("/getnew/addsome", handler.Postvalue).Methods("POST")
+	r.HandleFunc("/getnew", router.Getalldata).Methods("GET")
+	r.HandleFunc("/getnew/addsome", router.Postvalue).Methods("POST")
 
-	http.ListenAndServe(":2000", r)
+	http.ListenAndServe(":7000", r)
 
 }
